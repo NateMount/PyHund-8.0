0d1fa47ed3f482b5086d2d20c9f107aec8168a75
XXI <ngmount2021@gmail.com>
2026-01-30 10:57:46 -0500

    Added initial filtering and error logic to AutoMan

--- tools/automan.py
+++ tools/automan.py
@@ -11,22 +11,49 @@ from sys import argv
 class AutoMan:
 
     def __init__(self):
-        self.log = lambda msg: None
+        self.log = lambda *_: None
     
+    def build_from_arr(self, site_array:list[str]) -> None:
+        pass
+
+    def build_from_tag(self, tag_array:list[str]) -> None:
+        pass
+
     def exec(self, command:str) -> None:
         match command.lower():
             case 'build':
-                pass
+                if len(argv) < 3:
+                    print("[AutoMan::Err ~]: No build list or list of sites provided")
+                    return
+                
+                if argv[3].startswith('@'):
+                    try:
+                        site_arr:list[str] = open(argv[3][1:], 'r').read().split('\n')
+                    except FileNotFoundError:
+                        self.log("Err", f"Cannot open file @[{argv[3][1:]}]")
+                        return
+                else:
+                    site_arr:list[str] = argv[3:]
+                
+                self.build_from_arr(site_arr)
+
             case 'new':
-                pass
+                if len(argv) < 3:
+                    print("[AutoMan::Warn ~]: No tags provided, building default list")
+                    tags:list[str] = []
+                else:
+                    tags:list[str] = argv[3:]
+                
+                self.build_from_tag(tags)
+
             case 'tags':
-                pass
+                print("[AutoMan::IDX ~]: Tags")
+                # Display availible tags
+
             case _:
-                self.log(f"[AutoMan:Err ~]: Command {command} not recognized")
+                self.log("Err", f"Command {command} not recognized")
                 print("[AutoMan ~]:\n\nUsage:\n\tpython3 automan.py <command> @<sitelist_file> [/v]")
                 return
-
-
 
 
 def main() -> None:
@@ -37,7 +64,7 @@ def main() -> None:
 
     automan = AutoMan()
     if '/v' in argv:
-        automan.log = lambda msg: print(f"[AutoMan::Info ~]: {msg}")
+        automan.log = lambda status, msg: print(f"[AutoMan::{status} ~]: {msg}")
     
     automan.exec(argv[1])
 
