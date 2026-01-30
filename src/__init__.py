3cb2db3290cd21f31b263f24c435b8700efe14b0
XXI <ngmount2021@gmail.com>
2026-01-30 14:47:05 -0500

    Added argument parsing to '__init__.py' with initial args

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+# [PyHund::src - init]
+
+# === Imports
+from argparse import ArgumentParser
+
+# === Parser Setup
+
+# Build Parser
+PARSER:ArgumentParser = ArgumentParser(
+    prog="PyHund",
+    description="Adaptive and modular OSINT web-scraping tool used to detect user-instances across the internet",
+    prefix_chars='/'
+)
+
+# Add Args
+PARSER.add_argument('usernames', nargs='+')
+PARSER.add_argument('/verbose', '/v', help="Enable verbose output for program execution", action='store_true')
+PARSER.add_argument('/output', '/o', help="Sets expected output file in the form of name.ext", default='Report.txt')
+PARSER.add_argument('/threads', '/t', help="Set maximum number of threads to be issued during program execution", type=int, default=3)
+
+# Pre-build args 
+args = PARSER.parse_args()
