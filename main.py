1b2c058306385773f16117c9fedeef851f2e9a07
XXI <ngmount2021@gmail.com>
2026-02-02 16:00:44 -0500

    Added initial logic for loading and running PyHundScanner

--- main.py
+++ main.py
@@ -2,8 +2,20 @@
 # Author(s): Nate Mount ( @NateMount )
 # Version: 8.0.0 - InDev
 
+# === Imports
+
+from src import args
+from src.scanner import PyHundScanner
+
+# === Main Execution
+
 def main():
-    pass
+    pyhund_scanner:PyHundScanner = PyHundScanner(args.usernames)
+    if args.verbose:
+        pyhund_scanner.log = lambda *msg: print(f"[PyHund.Scanner ~]: {msg}")
+
+    pyhund_scanner.load_manifest(args.manifest)
+    pyhund_scanner.run(args.usernames, args.threads or 1)
 
 if __name__ == '__main__':
     main()
