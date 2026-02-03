397d289fd222b11a0ec90fc65b4b0bae02f4510e
XXI <ngmount2021@gmail.com>
2026-02-03 10:07:41 -0500

    Implemented initial methods ('_validate_response' & '_scan_site_block') in preperation for implementation of multithreading

--- src/scanner.py
+++ src/scanner.py
@@ -4,6 +4,7 @@
 
 from json import load
 from threading import Lock, Thread
+from requests import get, RequestException, Response
 
 # === Globals
 LOCK:Lock = Lock()
@@ -20,9 +21,8 @@ class PyHundScanner:
     
     def run(self, user_instances:list[str], thread_count:int):
         """
-        Executes scan of all provided UserInstances
+        Executes scan of all provided user instances
         
-        :param self: Description
         :param user_instances: Iterable collection of strings, each representing a single user instance
         :param thread_count: Maximum number of threads to be allocated to scan execution
         """
@@ -40,3 +40,93 @@ class PyHundScanner:
             exit(-1)
     
     def _scan_instance(self, user_instance:str) -> None:
+        """
+        Scans an array of sites derived from Manifest for selected user instance
+        
+        :param user_instance: String representing targeted user instance
+        """
+
+        self.scan_data[user_instance] = {}
+
+        for site in self.manifest.keys():
+            pass
+    
+    @staticmethod
+    def _validate_response(response_data:Response, user_instance:str, verification_method:str, verification_keys:dict) -> str:
+        """
+        Uses established verification methods and keys to tag this connection as valid / invalid / unknown
+        
+        :param response_data: Response data from site connection
+        :param user_instance: User instance used for search
+        :param verification_method: Method used to identify if user does exist on site
+        :param verification_keys: Mapping of 'invalid' and 'valid' status to their respective keys based on verification method
+        :return: Valid / Invalid / Unknown 
+        """
+
+
+        match verification_method:
+            case 'status-code':
+                if response_data.status_code == verification_keys['valid']:
+                    return 'Valid'
+                return 'Invalid'
+
+            case 'url':
+                if response_data.url.replace(user_instance, '@@@@@') == verification_keys['valid']:
+                    return 'Valid'
+                return 'Invalid'
+
+            case 'length':
+                response_len:int = len(response_data.text.replace(user_instance, '@@@@@'))
+                if abs(response_len - verification_keys['valid']) < abs(response_len - verification_keys['invalid']):
+                    return 'Valid'
+                return 'Invalid'
+
+            case 'key-string':
+                if verification_keys['invalid'] in response_data.text.replace(user_instance, '@@@@@'):
+                    return 'Invalid'
+                return 'Valid'
+
+            case _:
+                return 'Unknown'
+
+
+
+    def _scan_site_block(self, user_instance:str, site_block:list[str]) -> dict[str, tuple]:
+        """
+        Allows for fragmentation of scan space into threaded blocks, allowinf for faster 
+        compute speeds
+        
+        :param user_instance: String representing the user instance to be applied
+        :param site_block: List containing slice of all sites to be scanned
+        """
+
+        block_response_data:dict[str, tuple] = {}
+
+        for site_name in site_block:
+            self.log(f"Scanning: [{user_instance}@{site_name}]")
+            site_metadata:dict = self.manifest[site_name]
+            try:
+                response_data:Response = get(
+                    site_metadata[site_name]['url'].format(user_instance),
+                    headers=site_metadata['headers'],
+                    cookies=site_metadata['cookies']
+                )
+            except RequestException as err:
+                self.log(f"Error Encountered:\n\tError: {err}\n\tTarget: {site_metadata['url'].format(user_instance)}")
+                block_response_data[site_name] = ('Error', 'Cannot Connect to Site')
+                continue
+
+            block_response_data[site_name] = (
+                self._validate_response(
+                    response_data=response_data, 
+                    user_instance=user_instance,
+                    verification_method=site_metadata['check-type'], 
+                    verification_keys=site_metadata['criteria']
+                ),
+                site_metadata['url'].format(user_instance),
+                response_data.status_code,
+                site_metadata['check-type'],
+                site_metadata['criteria']
+            )
+
+        return block_response_data
