751808bcd805f9b0fd4580e0bb3eb142249b2624
Lgsx <ngmount2021@gmail.com>
2026-03-14 12:58:44 -0400

    Syntax Update

--- tools/pyhunter.py
+++ tools/pyhunter.py
@@ -6,11 +6,12 @@
 # === Imports
 
 from sys import argv
-from requests import Session, Response, RequestException
+
+from requests import RequestException, Response, Session
+
 
 # === PyHunter Class
 class PyHunter:
-
     USER_AGENTS = (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
@@ -19,26 +20,28 @@ class PyHunter:
         "Mozilla/5.0 (Linux; Android 11; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Mobile Safari/537.36",
     )
 
-    def __init__(self, url:str = ""):
+    def __init__(self, url: str = ""):
         self.log = lambda msg: None
         self.session = Session()
         self.headers = {
             "User-Agent": "PyHunter/0.2",
             "Accept": "*/*",
-            "Connection": "keep-alive"
+            "Connection": "keep-alive",
         }
         self.url = url
 
-    def connect(self, url:str) -> Response:
+    def connect(self, url: str) -> Response | None:
         self.log(f"Attemtping connection to {url}")
 
         try:
-            response:Response = self.session.get(url, headers=self.headers, timeout=10)
+            response: Response = self.session.get(url, headers=self.headers, timeout=10)
             if response.status_code not in (403, 429):
                 self.log("Connection Established")
                 return response
-            
-            self.log(f"Connection Blocked [{response.status_code}], Rotating User Agents...")
+
+            self.log(
+                f"Connection Blocked [{response.status_code}], Rotating User Agents..."
+            )
         except RequestException as err:
             self.log(f"Connection Error: {err}")
             pass
@@ -52,117 +55,140 @@ class PyHunter:
                     self.log("Connection Established")
                     return response
             except RequestException as err:
-                self.log(f"New UA Failed [{response.status_code}]")
+                self.log(f"New UA Failed [{err}]")
                 continue
-        
+
         self.log("Failed to Connect With All Availible User-Agents")
         return None
 
-    def analyse_verification_method(self, valid_user:str, invalid_user:str = "PH000XIP12") -> dict:
+    def analyse_verification_method(
+        self, valid_user: str, invalid_user: str = "PH000XIP12"
+    ) -> dict:
 
-        valid_response:Response = self.connect(self.url.format(valid_user))
+        valid_response: Response | None = self.connect(self.url.format(valid_user))
         if valid_response is None:
-            return {'Error': 'Cannot Connect to Valid User Profile'}
-        
-        invalid_response:Response = self.connect(self.url.format(invalid_user))
+            return {"Error": "Cannot Connect to Valid User Profile"}
+
+        invalid_response: Response | None = self.connect(self.url.format(invalid_user))
         if invalid_response is None:
-            return {'Error': 'Cannot Connect to InValid User Profile'}
-        
-        mapping:dict = {
+            return {"Error": "Cannot Connect to InValid User Profile"}
+
+        mapping: dict = {
             "url": self.url,
             "headers": self.headers,
             "cookies": self.session.cookies.get_dict(),
             "check-type": None,
-            "criteria": {}
+            "criteria": {},
         }
 
-        method_found:bool = False
+        method_found: bool = False
 
         if valid_response.status_code != invalid_response.status_code:
-            mapping['check-type'] = 'status-code'
-            mapping['criteria'] = {
-                'valid': valid_response.status_code, 
-                'invalid':invalid_response.status_code
+            mapping["check-type"] = "status-code"
+            mapping["criteria"] = {
+                "valid": valid_response.status_code,
+                "invalid": invalid_response.status_code,
             }
-            self.log(f"Method Found: Status Code +[{valid_response.status_code}] VS -[{invalid_response.status_code}]")
+            self.log(
+                f"Method Found: Status Code +[{valid_response.status_code}] VS -[{invalid_response.status_code}]"
+            )
             method_found = True
-        
-        if not method_found and valid_response.url.replace(valid_user, '@@@@@') != invalid_response.url.replace(invalid_user, '@@@@@'):
-            mapping["check-type"] = 'url'
-            mapping['criteria'] = {
-                'valid': valid_response.url.replace(valid_user, '@@@@@'),
-                'invalid': invalid_response.url.replace(invalid_user, '@@@@@')
-            }
-            method_found = True
-        
-        if not method_found and (valid_len := len(valid_response.text.replace(valid_user, '@@@@@'))) != (invalid_len := len(invalid_response.text.replace(invalid_user, '@@@@@'))):
-            mapping['check-type'] = 'length'
-            mapping['criteria'] = {
-                'valid': valid_len,
-                'invalid': invalid_len
+
+        if not method_found and valid_response.url.replace(
+            valid_user, "@@@@@"
+        ) != invalid_response.url.replace(invalid_user, "@@@@@"):
+            mapping["check-type"] = "url"
+            mapping["criteria"] = {
+                "valid": valid_response.url.replace(valid_user, "@@@@@"),
+                "invalid": invalid_response.url.replace(invalid_user, "@@@@@"),
             }
             method_found = True
-        
+
+        if not method_found and (
+            valid_len := len(valid_response.text.replace(valid_user, "@@@@@"))
+        ) != (invalid_len := len(invalid_response.text.replace(invalid_user, "@@@@@"))):
+            mapping["check-type"] = "length"
+            mapping["criteria"] = {"valid": valid_len, "invalid": invalid_len}
+            method_found = True
+
         if not method_found:
-            for key_string in ('404', 'Error', 'Not Found'):
-                if key_string in invalid_response.text and key_string not in valid_response.text:
-                    mapping['check-type'] = 'key-string'
-                    mapping['criteria'] = {
-                        'valid': '@@@Unknown@@@',
-                        'invalid': key_string
+            for key_string in ("404", "Error", "Not Found"):
+                if (
+                    key_string in invalid_response.text
+                    and key_string not in valid_response.text
+                ):
+                    mapping["check-type"] = "key-string"
+                    mapping["criteria"] = {
+                        "valid": "@@@Unknown@@@",
+                        "invalid": key_string,
                     }
                 else:
                     self.log("Failed to find Mapping")
-                    return {'Error': 'No Valid Method Found'}
-        
+                    return {"Error": "No Valid Method Found"}
+
         if method_found:
             if not self._test_cookies(valid_user, mapping):
-                mapping['cookies'] = {}
+                mapping["cookies"] = {}
             return mapping
 
-        return {'Error': 'No Valid Method Found'}
+        return {"Error": "No Valid Method Found"}
 
-    def _test_cookies(self, username:str, mapping:dict) -> bool:
+    def _test_cookies(self, username: str, mapping: dict) -> bool:
         tmp_session = Session()
 
         try:
-            response = tmp_session.get(self.url.format(username), headers=self.headers, timeout=10)
+            response = tmp_session.get(
+                self.url.format(username), headers=self.headers, timeout=10
+            )
         except RequestException:
             return True
-        
-        match mapping['check-type']:
-            case 'status_code':
-                if response.status_code != mapping['criteria']['valid']:
+
+        match mapping["check-type"]:
+            case "status_code":
+                if response.status_code != mapping["criteria"]["valid"]:
                     self.log(f"Cookies Required, Status Code [{response.status_code}]")
                     return True
-            
-            case 'url':
-                if response.url.replace(username, '@@@@@') != mapping['criteria']['valid']:
-                    self.log(f"Cookies Required, URL [{response.url.replace(username, '@@@@@')}]")
+
+            case "url":
+                if (
+                    response.url.replace(username, "@@@@@")
+                    != mapping["criteria"]["valid"]
+                ):
+                    self.log(
+                        f"Cookies Required, URL [{response.url.replace(username, '@@@@@')}]"
+                    )
                     return True
-            
-            case 'length':
-                current_len:int = len(response.text)
-                if abs(current_len - mapping['criteria']['valid']) > abs(current_len - mapping['criteria']['invalid']):
-                    self.log(f"Cookies Required, Len [{current_len} is Closer to InValid Len than Valid Len]")
+
+            case "length":
+                current_len: int = len(response.text)
+                if abs(current_len - mapping["criteria"]["valid"]) > abs(
+                    current_len - mapping["criteria"]["invalid"]
+                ):
+                    self.log(
+                        f"Cookies Required, Len [{current_len} is Closer to InValid Len than Valid Len]"
+                    )
                     return True
         self.log("No Cookies Required")
         return False
 
-# === Main Program 
+
+# === Main Program
 def main() -> None:
     if len(argv) < 3:
-        print("[PyHunter ~]:\n\nUsage:\n\tpython3 pyhunder.py <url> <valid_username> [/v]")
-        return 
-    
-    # args[1] should be the provided URL
-    hunter:PyHunter = PyHunter(argv[1])
+        print(
+            "[PyHunter ~]:\n\nUsage:\n\tpython3 pyhunder.py <url> <valid_username> [/v]"
+        )
+        return
 
-    if '/v' in argv:
+    # args[1] should be the provided URL
+    hunter: PyHunter = PyHunter(argv[1])
+
+    if "/v" in argv:
         hunter.log = lambda msg: print(f"[PyHunter::Info ~]: {msg}")
-    
+
     # args[2] should be valid username
     print(hunter.analyse_verification_method(argv[2]))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
