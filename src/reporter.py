ab59b7509e95f6b0bfb77fcb64241342f15281ce
Lgsx <ngmount2021@gmail.com>
2026-03-15 22:05:15 -0400

    Added initial methods for output formats

--- src/reporter.py
+++ src/reporter.py
@@ -1,20 +1,37 @@
 # [PyHund::Reporter ~]
 
+# === Imports
+
+from json import dump
+
+# === PyHund Reporter Class
+
 class PyHundReporter:
     
-    def build_report(self, raw_scan:dict, output_format:str = 'txt'):
+    def build_report(self, raw_scan:dict, output_format:str = 'txt', output_file:str = 'ScanResults'):
         match output_format:
-            case 'txt':
-                pass
+            case 'txt'  :  self.to_text(raw_scan, output_file)
+            case 'csv'  :  self.to_csv(raw_scan, output_file)
+            case 'json' :  self.to_json(raw_scan, output_file)
+            case 'yaml' :  self.to_yaml(raw_scan, output_file)
+            case _      :  print(raw_scan)
 
-            case 'csv':
-                pass
+    @staticmethod
+    def to_txt(raw_scan:dict, output_file:str) -> None:
+        
+        with open(output_file, 'w') as f:
+            pass
 
-            case 'json':
-                pass
+    @staticmethod
+    def to_csv(raw_scan:dict, output_file:str) -> None:
+        
+        with open(output_file, 'w') as f:
+            pass
 
-            case 'yaml':
-                pass
+    @staticmethod
+    def to_json(raw_scan:dict, output_file:str) -> None:
+        dump(raw_scan, output_file)
 
-            case _:
-                print(raw_scan)
+    @staticmethod
+    def to_yaml(raw_scan:dict, output_file:str) -> None:
+        pass
